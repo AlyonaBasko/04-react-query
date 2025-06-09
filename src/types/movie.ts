743b14b194603ce44b90cb3493fa8bbabe1bf372
alona <alenabask93@gmail.com>
2025-06-09 20:55:18 +0200

    update movie.ts

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -2,6 +2,7 @@ export interface Movie {
   id: number;
   title: string;
   overview: string;
+  backdrop_path: string; 
   poster_path: string | null;
   release_date: string;
   vote_average: number;
